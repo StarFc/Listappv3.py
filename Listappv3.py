1ea311557e948c46fd703c89d561124d21a1ce56
StarFc <79867299+StarFc@users.noreply.github.com>
2021-03-18 10:17:41 -0700

    Add files via upload

--- Listappv3.py
+++ Listappv3.py
@@ -9,14 +9,24 @@ def mainProgram():
         print("Please choose front the following options. Type the number of the choice")
         choice = input("""1. Add to a list,
 2. Return a value in a list,
-3. Random search
-4. Quit    """)
+3. Add a bunch of numbers! 
+4. Random search
+5. Print List 
+6. Quit    """)
         if choice == "1":
             addToList()
         elif choice == "2":
             indexValues()
         elif choice == "3":
+            addABunch()
+        elif choice == "4":
+            randomSearch()
+        elif choice == "5":
+            print(myList)
+        else:
             break
+            
+            
 
 def addToList():
     print("Adding to a list! Great choice!")
@@ -24,9 +34,20 @@ def addToList():
     myList.append(int(newItem))
     #we need to think about errors!
 
+def addABunch():
+    print("we're gonna add a bunch of number to your list!")
+    numToAdd = input("How many new integers would you like to add?   ")
+    numRange = input("And how high would you like these numbers to go   ")
+    for x in range(0, int(numToAdd)):
+        myList.append(random.randint(0, int(numRange)))
+    print("Your list is complete!")
+
 def randomSearch():
     print("oH, iM sO rAnDom!!!")
     myList[random.randint(0, len(myList) -1)]
+
+def LinearSearch():
+    print("We're going to go through this list one item at a time!") 
 
 
 def indexValues():
@@ -34,6 +55,5 @@ def indexValues():
     indexPos = input("Type an index position here:    ")
     print(myList[int(indexPost)])
 
-
-if__name__ == "__main__":
+if __name__ == "__main__":
     mainProgram()
